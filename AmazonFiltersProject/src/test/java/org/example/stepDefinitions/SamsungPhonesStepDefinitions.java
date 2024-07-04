4687eb696518b8ee377da9c9b4897dd0b0949933
ElinaBkr <info.bakerelina@gmail.com>
2024-07-04 20:28:24 +0100

    Update SamsungPhonesStepDefinitions.java

--- AmazonFiltersProject/src/test/java/org/example/stepDefinitions/SamsungPhonesStepDefinitions.java
+++ AmazonFiltersProject/src/test/java/org/example/stepDefinitions/SamsungPhonesStepDefinitions.java
@@ -14,6 +14,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
+
 public class SamsungPhonesStepDefinitions {
     private WebDriver driver;
     private HomePage homePage;
